f4767e748e8a339977f3f911682f525a1924796b
Henrik Fricke <henrikfricke@web.de>
2024-10-28 11:15:29 +0100

    feat: vercel setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vercelPreset } from '@vercel/remix/vite';
 
 declare module "@remix-run/node" {
   interface Future {
@@ -18,6 +19,7 @@ export default defineConfig({
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
       },
+      presets: [vercelPreset],
     }),
     tsconfigPaths(),
   ],
