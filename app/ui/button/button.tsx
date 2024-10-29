e5ad9428ce521161624aebe55c8de3e9e3b1c4d2
Henrik Fricke <henrikfricke@web.de>
2024-10-29 15:41:01 +0100

    fix: linting issues

--- app/ui/button/button.tsx
+++ app/ui/button/button.tsx
@@ -1,14 +1,9 @@
-import React from "react";
+import { ButtonHTMLAttributes, FC } from "react";
 import { clsx } from "clsx/lite";
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button: React.FC<ButtonProps> = ({
-  children,
-  className,
-  ...props
-}) => {
+export const Button: FC<ButtonProps> = ({ children, className, ...props }) => {
   const classes = clsx("rounded bg-blue-500 text-white px-4 py-3", className);
 
   return (
