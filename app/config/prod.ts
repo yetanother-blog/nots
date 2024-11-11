3d842918e23121b69fb2be78ec68401325d82168
Henrik Fricke <henrikfricke@web.de>
2024-11-11 15:18:20 +0100

    feat: test database (#23)

---
+++ app/config/prod.ts
@@ -0,0 +1,10 @@
+import env from 'env-var';
+import { Config } from ".";
+
+export function getConfigProd(): Config {
+  return {
+    database: {
+      url: env.get('DATABASE_URL').required().asString(),
+    }
+  }
+}
