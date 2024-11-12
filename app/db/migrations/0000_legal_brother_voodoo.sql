f1d77a87d22410562ae7f8bc17453b3dca099d9d
Henrik Fricke <henrikfricke@web.de>
2024-11-12 17:34:30 +0100

    refa(schema): change content column to jsonb

--- app/db/migrations/0000_legal_brother_voodoo.sql
+++ app/db/migrations/0000_legal_brother_voodoo.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS "documents" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"content" text DEFAULT '' NOT NULL,
+	"content" jsonb DEFAULT '[]'::jsonb NOT NULL,
 	"createdAt" timestamp DEFAULT now() NOT NULL,
 	"updatedAt" timestamp DEFAULT now() NOT NULL,
 	"deletedAt" timestamp
