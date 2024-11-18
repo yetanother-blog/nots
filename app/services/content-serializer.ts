5497bf645505caa3b8d540b8dc77ac7ef3c0c421
Henrik Fricke <henrikfricke@web.de>
2024-11-18 11:15:23 +0100

    refa(content serializer): re-use code for heading 1 to heading3

--- app/services/content-serializer.ts
+++ app/services/content-serializer.ts
@@ -3,6 +3,7 @@ import {
   ContentInline,
   ContentInlineText,
 } from '~/types/content';
+import invert from 'lodash.invert';
 
 const contentBlockTypeMapping: Record<string, ContentBlock['type']> = {
   H1: 'heading1',
@@ -69,22 +70,15 @@ export function toHtml(content: ContentBlock[]): string {
   const div = document.createElement('div');
 
   for (const block of content) {
-    if (block.type === 'heading1') {
-      const h1 = document.createElement('h1');
-      h1.append(...toHtmlInline(block.nodes));
-      div.appendChild(h1);
-    }
-
-    if (block.type === 'heading2') {
-      const h2 = document.createElement('h2');
-      h2.append(...toHtmlInline(block.nodes));
-      div.appendChild(h2);
-    }
-
-    if (block.type === 'heading3') {
-      const h3 = document.createElement('h3');
-      h3.append(...toHtmlInline(block.nodes));
-      div.appendChild(h3);
+    if (
+      block.type === 'heading1' ||
+      block.type === 'heading2' ||
+      block.type === 'heading3'
+    ) {
+      const tagName = invert(contentBlockTypeMapping)[block.type];
+      const heading = document.createElement(tagName);
+      heading.append(...toHtmlInline(block.nodes));
+      div.appendChild(heading);
     }
 
     if (block.type === 'paragraph') {
